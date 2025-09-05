6939936daa2de7e515587ae83cfbec9dd66be896
naikshreshta97 <shreshtanaik69@gmail.com>
2025-09-05 16:59:55 +0530

    Add files via upload

---
+++ Python/Activities/activity8.py
@@ -0,0 +1,13 @@
+num=[10,20,30,20]
+
+if num[0]==num[-1]:
+    print("True")
+else:
+    print("False")
+
+
+num1=input("Enter numbers with comma: ").split(",")
+if int(num1[0])==int(num1[-1]):
+    print("True1")
+else:
+    print("False1")
